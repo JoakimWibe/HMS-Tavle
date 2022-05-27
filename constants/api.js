f70a7abd67d88d3562a23719cd47abf6b93b4eab
JoakimWibe <71314249+JoakimWibe@users.noreply.github.com>
2022-05-27 14:00:39 +0200

    Update api.js

--- constants/api.js
+++ constants/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://hms-tavle.herokuapp.com";
+const BASE_URL = process.env.BASE_URL;
 
 export const FAQ_URL = BASE_URL + "/api/faqs/";
 export const EXAMPLES_URL = BASE_URL + "/api/eksempler/";
